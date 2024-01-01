8c4bcb14dd1915420a44340602305e1599a17a0e
Jack <97347951+Bayyeee@users.noreply.github.com>
2024-01-01 17:33:34 +0800

    Update createlevel.php
    Hapus Token Telegram

--- createlevel.php
+++ createlevel.php
@@ -18,30 +18,4 @@ if ($result_kecepatan_air->num_rows > 0) {
 // Masukkan data tinggi air ke dalam tabel ketinggian_air
 $sql_tinggi_air = "INSERT INTO ketinggian_air (tinggi_air, waktu_input) VALUES ('$tinggi_air', NOW())";
 
-if ($koneksi->query($sql_tinggi_air) === TRUE) {
-    echo 'Data tinggi air berhasil disimpan ';
-
-    // Cek jika ketinggian air kurang dari 50cm
-    if ($tinggi_air < 50) {
-        // Notifikasi ke Telegram
-        $telegrambot = 'bot6424345808:AAHnMSaAjAoPTm2hVY1Jwq2mwiptUn08TU0';
-        $user = '1441844858';
-//        $pesan = 'Peringatan: Ketinggian air ' . $tinggi_air . 'cm. Kecepatan air ' . $flow_rate . 'm/s. Segera persiapkan diri!';
-        $pesan = '🚨 Peringatan 🚨: Ketinggian air saat ini ' . $tinggi_air . ' cm dan kecepatan air ' . $flow_rate . ' l/mil.';
-
-        // Kirim notifikasi ke Telegram
-        kirim($pesan, $telegrambot, $user);
-    }
-} else {
-    echo 'Error: ' . $sql_tinggi_air . '<br>' . $koneksi->error;
-}
-
 $koneksi->close();
-
-// Fungsi untuk mengirim pesan ke Telegram
-function kirim($pesan, $bot, $chat)
-{
-    $url = 'https://api.telegram.org/'.$bot.'/sendMessage?chat_id='.$chat.'&text='.$pesan;
-    $result = file_get_contents($url, true);
-    return $result;
-}
